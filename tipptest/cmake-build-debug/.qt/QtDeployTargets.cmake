7398a43cc058a8bce64518b2ee5ceec32be72031
krustus <78026663+badboy00neu@users.noreply.github.com>
2025-04-04 09:27:21 +0200

    gitignore

--- tipptest/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_tipptest_FILE /Users/justus/gitProjects/tipptest/tipptest/cmake-build-debug/tipptest.app/Contents/MacOS/tipptest)
-set(__QT_DEPLOY_TARGET_tipptest_TYPE EXECUTABLE)
